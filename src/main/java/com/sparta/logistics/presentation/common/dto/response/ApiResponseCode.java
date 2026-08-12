90274ad173912caad0bffc0b3b0e47b2f3456604
woohyun23 <parkwh0823@gmail.com>
2026-08-12 15:55:01 +0900

    fix: 공통 템플릿 적용 정리

--- src/main/java/com/sparta/logistics/presentation/common/dto/response/ApiResponseCode.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.logistics.presentation.common.dto.response;
-
-import org.springframework.http.HttpStatus;
-
-public interface ApiResponseCode {
-    HttpStatus getStatus();
-    String getMessage();
-}
--- src/test/java/com/sparta/logistics/OrderServiceApplicationTests.java
+++ src/test/java/com/sparta/logistics/OrderServiceApplicationTests.java
@@ -3,7 +3,18 @@ package com.sparta.logistics;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.cloud.config.enabled=false",
+        "eureka.client.enabled=false",
+        "spring.rabbitmq.host=localhost",
+        "spring.rabbitmq.port=5672",
+        "spring.rabbitmq.username=guest",
+        "spring.rabbitmq.password=guest",
+        "spring.autoconfigure.exclude="
+                + "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,"
+                + "org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration,"
+                + "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+})
 class OrderServiceApplicationTests {
 
     @Test
